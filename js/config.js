e1aabffb2133c49fbb1f9c0340dc68a3197fc2f8
muksol2003 <muksol2003@proton.me>
2026-05-31 18:58:15 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const GEMINI_API_KEY = "AQ.Ab8RN6Lnj2BjByiGjz7mAoXwplLGNpL2ae7Fhp2BWjFIaTPxYw";
+
+const GEMINI_MODEL = "gemini-2.0-flash";
+
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}:generateContent?key=${GEMINI_API_KEY}`;
